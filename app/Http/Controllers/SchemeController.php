dd64f95ba766fcbf5fa41d6bbace1cf7cb0e8aa6
vagrant <vagrant@homestead>
2017-10-07 10:24:56 +0000

    case sensitive class

--- app/Http/Controllers/SchemeController.php
+++ app/Http/Controllers/SchemeController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\scheme;
+use App\Scheme;
 use App\Coin;
 use Illuminate\Http\Request;
 use adman9000\Bittrex\Bittrex;
--- app/scheme.php
+++ app/scheme.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class scheme extends Model
+class Scheme extends Model
 {
     //
     protected $guarded = [];
